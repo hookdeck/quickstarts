77723f7e8f399e1696b78c06f0561d26e2cf51fe
Phil Leggetter <phil@leggetter.co.uk>
2023-11-18 20:38:37 +0000

    chore: ts inbound add logging + JSON response

--- typescript/inbound/README.md
+++ typescript/inbound/README.md
@@ -5,3 +5,17 @@ Express.js.
 
 - Follow the [Hookdeck Inbound Webhook Quickstart](#)
 - Check out the [Hookdeck docs](https://hookdeck.com/docs)
+
+## Get the code
+
+```sh
+npx degit hookdeck/quickstarts/typescript/inbound hookdeck-ts-inbound
+cd hookdeck-ts-inbound
+```
+
+## Run the app
+
+```sh
+npm i
+npm run dev
+```
--- typescript/inbound/index.ts
+++ typescript/inbound/index.ts
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Application = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3030;
 
 app.post("/webhook", (req: Request, res: Response) => {
-  res.send("OK");
+  console.log({ received: new Date().toISOString(), body: req.body });
+
+  res.json({ order_status: "ACCEPTED" });
 });
 
 app.listen(port, () => {
