7799bbab89f49b33cd6e5d48dfd009494198f449
Phil Leggetter <phil@leggetter.co.uk>
2025-02-10 15:46:49 +0000

    chore: remove Hookdeck TS SDK from inbound

--- typescript/inbound/index.ts
+++ typescript/inbound/index.ts
@@ -7,7 +7,7 @@ declare module 'express-serve-static-core' {
 }
 
 import dotenv from "dotenv";
-import { verifyWebhookSignature } from "@hookdeck/sdk/webhooks";
+import crypto from "crypto";
 
 dotenv.config();
 
@@ -15,18 +15,32 @@ const HOOKDECK_WEBHOOK_SECRET = process.env.HOOKDECK_WEBHOOK_SECRET;
 
 const app: Application = express();
 
-const verify = async (req: Request, res: Response, next: NextFunction) => {
+app.use(express.json({
+  verify: (req: Request, res: Response, buf: Buffer, encoding: string) => {
+    req.rawBody = buf.toString();
+  }
+}));
+
+const verifyHookdeckSignature = async (req: Request, res: Response, next: NextFunction) => {
   if(!HOOKDECK_WEBHOOK_SECRET) {
     console.warn("No HOOKDECK_WEBHOOK_SECRET found in environment variables. Skipping verification.")
     next();
     return;
   }
 
-  const verified = await verifyWebhookSignature({
-    headers: req.headers as { [key: string]: string; },
-    rawBody: req.rawBody,
-    signingSecret: HOOKDECK_WEBHOOK_SECRET
-  });
+  // Extract x-hookdeck-signature and x-hookdeck-signature-2 headers from the request
+  const hmacHeader = req.get("x-hookdeck-signature");
+  const hmacHeader2 = req.get("x-hookdeck-signature-2");
+
+  // Create a hash based on the parsed body
+  const hash = crypto
+    .createHmac("sha256", HOOKDECK_WEBHOOK_SECRET)
+    .update(req.rawBody)
+    .digest("base64");
+
+  // Compare the created hash with the value of the x-hookdeck-signature 
+  // and x-hookdeck-signature-2 headers
+  const verified = (hash === hmacHeader || (hmacHeader2 && hash === hmacHeader2));
 
   if(verified) {
     next();
@@ -36,15 +50,9 @@ const verify = async (req: Request, res: Response, next: NextFunction) => {
   }
 }
 
-app.use(express.json({
-  verify: (req: Request, res: Response, buf: Buffer, encoding: string) => {
-    req.rawBody = buf.toString();
-  }
-}));
-
 const port = process.env.PORT || 3030;
 
-app.post("*", verify, (req: Request, res: Response) => {
+app.post("*", verifyHookdeckSignature, (req: Request, res: Response) => {
   console.log({
     webhook_received: new Date().toISOString(), 
     path: req.path, 
