b3641d189e71b3765cf69b7a2f4438f1ce2a5ce7
Phil Leggetter <phil@leggetter.co.uk>
2025-02-10 16:06:53 +0000

    chore: remove SDK usage from Deno example

--- typescript/create-connection/index-deno.ts
+++ typescript/create-connection/index-deno.ts
@@ -1,32 +1,72 @@
 import { load } from "https://deno.land/std@0.223.0/dotenv/mod.ts";
 const env = await load();
-import { HookdeckClient } from '@hookdeck/sdk';
 
-const hookdeck = new HookdeckClient({
-  token: env.HOOKDECK_API_KEY
-});
+interface SourceConfig {
+  name: string;
+}
 
-const connection = await hookdeck.connection.upsert({
+interface DestinationConfig {
+  name: string;
+  url?: string;
+  cli_path?: string;
+}
+
+interface ConnectionConfig {
+  name: string;
+  source: SourceConfig;
+  destination: DestinationConfig;
+}
+
+interface ConnectionResponse {
+  source: SourceConfig & { url?: string; cli_path?: string };
+  destination: DestinationConfig;
+}
+
+const createHookdeckConnection = async (
+  connectionConfig: ConnectionConfig
+): Promise<ConnectionResponse> => {
+  const response = await fetch(
+    "https://api.hookdeck.com/2024-09-01/connections",
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${env.HOOKDECK_API_KEY}`,
+      },
+      body: JSON.stringify(connectionConfig),
+    }
+  );
+
+  return await response.json();
+};
+
+const connection = await createHookdeckConnection({
   name: "inbound-example",
   source: {
-    name: "inbound"
+    name: "inbound",
   },
   destination: {
     name: "outbound",
-    url: "https://mock.hookdeck.com"
-  }
+    url: "https://mock.hookdeck.com",
+  },
 });
 
-console.log("Created or updated Connection. Source URL:", connection.source.url);
+console.log(
+  "Created or updated Connection. Source URL:",
+  connection.source.url
+);
 
-const publishResult = await fetch(connection.source.url, {
-  method: 'POST',
+const publishResult = await fetch(connection.source.url!, {
+  method: "POST",
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
   body: JSON.stringify({
-    text: "Hello, World!"
-  })
+    text: "Hello, World!",
+  }),
 });
 
-console.log("Sent request to Connection Source. Response:", await publishResult.json());
+console.log(
+  "Sent request to Connection Source. Response:",
+  await publishResult.json()
+);
